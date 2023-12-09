b66baf989721db5aa345de64db7edcf0cb58fe47
yiliu <yl2682@gmail.com>
2023-12-09 22:56:02 +0800

    sencond commit

--- MVC/Controllers/HomeController.cs
+++ MVC/Controllers/HomeController.cs
@@ -1,12 +1,41 @@
-﻿using System.Diagnostics;
+﻿using System.ComponentModel.DataAnnotations;
+using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
 using MVC.Models;
+using MvcMovie.Models;
+using System;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+using System.Text;
 
 namespace MVC.Controllers;
 
 public class HomeController : Controller
 {
     private readonly ILogger<HomeController> _logger;
+
+
+    private List<Movie> movies = new List<Movie>
+    {
+        new Movie { Id = 5,Title="Holy Grail"
+            , ReleaseDate=new DateTime(2023, 3, 11)}
+  
+
+    };
+
+
+    public IActionResult ExportToCSV()
+    {
+        var builder = new StringBuilder();
+        builder.AppendLine("Id,Title");
+        foreach (var movie in movies)
+        {
+            builder.AppendLine($"{movie.Id},{movie.Title}");
+        }
+        return File(Encoding.UTF8.GetBytes(builder.ToString()), "text/csv", "movies.csv");
+
+    }
 
     public HomeController(ILogger<HomeController> logger)
     {
---
+++ MVC/Controllers/movieController.cs
@@ -0,0 +1,182 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using MvcMovie.Data;
+using MvcMovie.Models;
+
+namespace MVC.Controllers
+{
+    public class movieController : Controller
+    {
+        private readonly MvcMovieContext _context;
+
+        public movieController(MvcMovieContext context)
+        {
+            _context = context;
+           
+        }
+
+
+        
+
+
+        public IActionResult ExportToCSV()
+        {
+            var builder = new StringBuilder();
+            builder.AppendLine("Id,Title");
+            foreach (var movie in _context.Movie)
+            {
+                builder.AppendLine($"{movie.Id},{movie.Title}");
+            }
+            return File(Encoding.UTF8.GetBytes(builder.ToString()), "text/csv", "movies.csv");
+
+        }
+
+
+        // GET: movie
+        public async Task<IActionResult> Index()
+        {
+              return _context.Movie != null ? 
+                          View(await _context.Movie.ToListAsync()) :
+                          Problem("Entity set 'MvcMovieContext.Movie'  is null.");
+        }
+
+        // GET: movie/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null || _context.Movie == null)
+            {
+                return NotFound();
+            }
+
+            var movie = await _context.Movie
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (movie == null)
+            {
+                return NotFound();
+            }
+
+            return View(movie);
+        }
+
+        // GET: movie/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: movie/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,Title,ReleaseDate,Genre,Price")] Movie movie)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(movie);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(movie);
+        }
+
+        // GET: movie/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null || _context.Movie == null)
+            {
+                return NotFound();
+            }
+
+            var movie = await _context.Movie.FindAsync(id);
+            if (movie == null)
+            {
+                return NotFound();
+            }
+            return View(movie);
+        }
+
+        // POST: movie/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,Title,ReleaseDate,Genre,Price")] Movie movie)
+        {
+            if (id != movie.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(movie);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!MovieExists(movie.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(movie);
+        }
+
+        // GET: movie/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null || _context.Movie == null)
+            {
+                return NotFound();
+            }
+
+            var movie = await _context.Movie
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (movie == null)
+            {
+                return NotFound();
+            }
+
+            return View(movie);
+        }
+
+        // POST: movie/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            if (_context.Movie == null)
+            {
+                return Problem("Entity set 'MvcMovieContext.Movie'  is null.");
+            }
+            var movie = await _context.Movie.FindAsync(id);
+            if (movie != null)
+            {
+                _context.Movie.Remove(movie);
+            }
+            
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool MovieExists(int id)
+        {
+          return (_context.Movie?.Any(e => e.Id == id)).GetValueOrDefault();
+        }
+    }
+}
